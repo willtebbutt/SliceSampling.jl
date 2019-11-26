ebd654c37caaa789944aa6c0087f4029cc36aaa2
Will Tebbutt <wt0881@my.bristol.ac.uk>
2019-11-26 16:20:00 +0000

    Use all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,5 @@ _psd_matrix(A) = Matrix(Symmetric(A * A' + I))
 
 @testset "SliceSampling" begin
     include("slice_sampler.jl")
-    # include("elliptical_slice_sampler.jl")
+    include("elliptical_slice_sampler.jl")
 end
